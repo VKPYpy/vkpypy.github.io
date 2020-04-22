e746b4b7cdfa09dcc2566403226aefeba50b7515
VKPYpy <49330371+VKPYpy@users.noreply.github.com>
2020-04-23 02:14:56 +0700

    Add files via upload

---
+++ precache-manifest.d3333bfd8b12ae02b853b21c5cc91112.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2e9d53aeed0d721c673aea98c09fbefd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f72f3d17fff1077d509e",
+    "url": "/static/css/2.86df01db.chunk.css"
+  },
+  {
+    "revision": "e80032acdfba33b38c78",
+    "url": "/static/css/main.e50aa3a3.chunk.css"
+  },
+  {
+    "revision": "f72f3d17fff1077d509e",
+    "url": "/static/js/2.22a4b804.chunk.js"
+  },
+  {
+    "revision": "21ced859ea2b2d6b856d461ad6c2afed",
+    "url": "/static/js/2.22a4b804.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e80032acdfba33b38c78",
+    "url": "/static/js/main.be76e4ad.chunk.js"
+  },
+  {
+    "revision": "33c7d4b2cb6cf49ea8fd",
+    "url": "/static/js/runtime-main.bf054b1d.js"
+  },
+  {
+    "revision": "3843ed88424935aa6be696317aa925d4",
+    "url": "/static/media/click.3843ed88.png"
+  }
+]);
